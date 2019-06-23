aca2ee640cacfa51fd0b33aecd2be0a0702a0ca8
Seon Ricks <skcirs@gmail.com>
2019-06-23 08:48:23 -0700

    JWT token and passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require('../../models/Users');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 
 // @route   POST api/users/register
@@ -60,9 +63,41 @@ router.post('/login', (req, res) => {
           email: 'User not found'
         });
       }
+
+      //Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            //User matched
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            };
+
+            //Sign a token
+            jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, 
+              (err, token) => {
+                return res.json({
+                  success: true, 
+                  token: 'Bearer ' + token
+                });
+              }
+            );
+          }
+          else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        })
     })
     .catch(err => console.log(err));
 })
 
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({msg: 'Success'});
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,6 +16,11 @@ mongoose.connect(db)
   .then(() => console.log('MongoDb connected'))
   .catch(err => console.log(err));
 
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => res.send('Hello World'));
 // route takes you to the js file:
